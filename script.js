9e5b7297696987f7da3a0b9fc62dee4f35c827ae
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2024-01-12 08:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
-  // write your code here
+	const word = 'qwertyuiop123asdfghjkl456zxcvbnm789QWERTYUIOP098LKJHGFDSA765ZXCVBNM1234'
+	let ans = '';
+	const len = word.length;
+	for(let i = 0; i < l; i++){
+		ans += word[Math.floor(Math.random() * len)];
+	}
+	return ans;
 }
 
 // Do not change the code below.
